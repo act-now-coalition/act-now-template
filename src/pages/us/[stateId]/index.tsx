74e396f55e1060d224a78160d004356abc435ace
Pablo Navarro <pnavarrc@gmail.com>
2022-07-18 16:23:09 -0700

    Test regions package

---
+++ src/pages/us/[stateId]/index.tsx
@@ -0,0 +1,71 @@
+import { GetStaticPaths, GetStaticProps } from "next";
+import { ParsedUrlQuery } from "querystring";
+import { Container, Typography } from "@mui/material";
+import { states, Region } from "@actnowcoalition/regions";
+import { assert } from "@actnowcoalition/assert";
+import { formatInteger } from "@actnowcoalition/number-format";
+
+// TODO: Export RegionJSON from the @actnowcoalition/region
+export interface RegionJSON {
+  regionId: string;
+  fullName: string;
+  shortName: string;
+  abbreviation: string;
+  urlFragment: string;
+  parent: RegionJSON | null;
+  population: number;
+}
+
+const RegionPage: React.FC<{ regionJSON: RegionJSON }> = ({ regionJSON }) => {
+  const region = Region.fromJSON(regionJSON);
+  return (
+    <Container>
+      <Typography variant="h1">{region.fullName}</Typography>
+      <Typography variant="body2">
+        {formatInteger(region.population)}
+      </Typography>
+    </Container>
+  );
+};
+
+export default RegionPage;
+
+interface StatePageProps extends ParsedUrlQuery {
+  stateId: string;
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { stateId } = params as StatePageProps;
+  /**
+   * TODO (region): We need a better way to represent urlSegments on
+   * a region so we can use the `params` here to find the region.
+   *
+   * In this case, the URL structure is given by the folder structure:
+   *
+   *  us
+   *   └── [stateId]
+   *       └── index.tsx
+   *
+   * which represents the URL `/us/${stateId}`. The URL segment of
+   * a state is just a string like "us/washington-wa", so we don't
+   * have a good way to match the `stateId` ("washington-wa") with
+   * the `urlSegment`.
+   *
+   * Here, I'm just hacking it by checking if the `urlFragment` includes
+   * the `stateId` (instead of finding it).
+   *
+   * For counties, the problem is slightly worse, we will have `stateId`
+   * and `countyId` as URL params, so we need a way to find the county
+   * using these 2 parameters.
+   */
+  const state = states.all.find((state) => state.urlFragment.includes(stateId));
+  assert(state, `State by urlSegment not found: ${stateId}`);
+  return { props: state.toJSON() };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = states.all.map((state) => ({
+    params: { stateId: state.urlFragment },
+  }));
+  return { paths, fallback: true };
+};
