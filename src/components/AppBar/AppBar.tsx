0fe8e3c99408f174b7f40a7b3380a9bdd55d3731
chasulin <44076375+chasulin@users.noreply.github.com>
2022-07-21 18:44:34 -0400

    Set up linting, prettier (#4)

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -1,13 +1,8 @@
-import {
-  Toolbar,
-  AppBar as MuiAppBar,
-  Box
-} from "@mui/material";
+import { Toolbar, AppBar as MuiAppBar, Box } from "@mui/material";
 
 const AppBar: React.FC = () => {
-
   return (
-    <MuiAppBar position="sticky" >
+    <MuiAppBar position="sticky">
       <Toolbar disableGutters>
         <Box p={2}>Logo</Box>
       </Toolbar>
