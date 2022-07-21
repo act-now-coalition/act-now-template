712ca76fc49bc3132d07b966fdcf662f99dcabcc
chasulin <44076375+chasulin@users.noreply.github.com>
2022-07-21 13:23:21 -0400

    remove unused fonts, unused import

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,4 +1,4 @@
-import { createTheme, Theme } from "@mui/material/styles";
+import { createTheme } from "@mui/material/styles";
 import palette from "./palette";
 import typography from "./typography";
 import components from "./components";
