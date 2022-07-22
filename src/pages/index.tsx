d2e400cddfd44ea544c19b77d0a63c6526f58346
Pablo Navarro <pnavarrc@gmail.com>
2022-07-21 17:09:57 -0700

    test legend

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
-import type { NextPage } from 'next'
-import Homepage from 'src/screens/Homepage';
+import type { NextPage } from "next";
+import Homepage from "src/screens/Homepage";
 
 const Home: NextPage = () => {
   return (
-      <main>
-       <Homepage />
-      </main>
-  )
-}
+    <main>
+      <Homepage />
+    </main>
+  );
+};
 
-export default Home
+export default Home;
--- src/screens/Homepage/Homepage.tsx
+++ src/screens/Homepage/Homepage.tsx
@@ -1,9 +1,46 @@
-import { Container } from "@mui/material";
+import { Box, Container } from "@mui/material";
+import { LegendThreshold } from "@actnowcoalition/ui-components";
+
+interface Item {
+  value: number;
+  label: string;
+  color: string;
+}
+
+const width = 300;
+const height = 40;
+const barHeight = 20;
+
+const items: Item[] = [
+  { value: 10, label: "10", color: "#90BE6D" },
+  { value: 20, label: "20", color: "#F9C74F" },
+  { value: 30, label: "30", color: "#F8961E" },
+  { value: 40, label: "40", color: "#E16420" },
+  { value: 50, label: "50", color: "#A10003" },
+];
+
+const getItemValue = (item: Item) => item.value;
+const getItemColor = (item: Item) => item.color;
+const getItemLabel = (item: Item) => item.label;
 
 const Homepage: React.FC = () => {
   return (
     <Container>
       Homepage
+      <Box>
+        <svg width={width} height={height}>
+          <LegendThreshold
+            items={items}
+            width={width}
+            height={height}
+            barHeight={barHeight}
+            borderRadius={barHeight / 2}
+            getItemValue={getItemValue}
+            getItemColor={getItemColor}
+            getItemLabel={getItemLabel}
+          />
+        </svg>
+      </Box>
     </Container>
   );
 };
