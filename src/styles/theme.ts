b710f90e84444aa12fcd0a4fa46369923f04e6fb
chasulin <44076375+chasulin@users.noreply.github.com>
2022-08-10 18:37:26 -0400

    Theming setup (#7)

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,16 +1,28 @@
-import { createTheme } from "@mui/material/styles";
-import palette from "./palette";
-import typography from "./typography";
-import components from "./components";
+import { createTheme } from '@mui/material/styles';
+import { deepmerge } from '@mui/utils';
+import { themeConfig as defaultThemeConfig } from '@actnowcoalition/ui-components';
+import palette from './palette';
+import typography from './typography';
+import components from './components';
 
 /**
  * Theme configuration variables
  * https://mui.com/customization/theming/#theme-configuration-variables
  */
-const theme = createTheme({
-    palette,
-    typography,
-    components,
-});
 
-export default theme
+const internalThemeConfig = {
+  palette,
+  typography,
+  components,
+};
+
+/**
+ * We take the theme config object containing the default
+ * Act Now design system (defaultThemeConfig), and we deep merge
+ * it with any project-specific theme variables in this repo's
+ * theme (internalThemeConfig).
+ */
+
+const theme = createTheme(deepmerge(defaultThemeConfig, internalThemeConfig));
+
+export default theme;
