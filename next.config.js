54461b574e7d3e6d600c273e682cc76974a41bfa
Michael Lehenbauer <mikelehen@gmail.com>
2022-08-30 18:27:06 -0700

    Enable  esmExternals in next config. Fixes #51. (#12)

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    esmExternals: "loose",
+  },
 };
 
 module.exports = withBundleAnalyzer(nextConfig);
