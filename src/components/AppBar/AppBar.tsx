9121257bf60b519b25860d9b9aa43df4fa88a882
Fai <fai@covidactnow.org>
2022-09-07 15:10:14 -0700

    Address comments

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -5,9 +5,9 @@ import {
   Box,
 } from "@mui/material";
 
-const AppBar: React.FC = (props: MuiAppBarProps) => {
+const AppBar: React.FC<MuiAppBarProps> = (props) => {
   return (
-    <MuiAppBar color={props.color} position="sticky">
+    <MuiAppBar position="sticky" {...props}>
       <Toolbar disableGutters>
         <Box p={2}>Logo</Box>
       </Toolbar>
