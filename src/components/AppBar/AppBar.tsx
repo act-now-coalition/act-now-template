05b36d5648f88d99ade70c363f083f1a60a5ff21
Fai <46338131+faihegberg@users.noreply.github.com>
2022-09-07 15:17:02 -0700

    Enable app bar to have non-primary color (#21)

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -1,8 +1,13 @@
-import { Toolbar, AppBar as MuiAppBar, Box } from "@mui/material";
+import {
+  Toolbar,
+  AppBar as MuiAppBar,
+  AppBarProps as MuiAppBarProps,
+  Box,
+} from "@mui/material";
 
-const AppBar: React.FC = () => {
+const AppBar: React.FC<MuiAppBarProps> = (props) => {
   return (
-    <MuiAppBar position="sticky">
+    <MuiAppBar position="sticky" {...props}>
       <Toolbar disableGutters>
         <Box p={2}>Logo</Box>
       </Toolbar>
