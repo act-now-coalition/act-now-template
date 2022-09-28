e2da953dfd6a8644c20b6d3bb8ec00532e1acd3c
Sean McClure <sean.mcclure17@gmail.com>
2022-09-28 14:12:12 -0400

    Add locaiton share pages for testing

--- src/pages/internal/share-image/[regionSlug]/index.tsx
+++ src/pages/internal/share-image/[regionSlug]/index.tsx
@@ -2,29 +2,35 @@ import type { GetServerSideProps, NextPage } from "next";
 import { ParsedUrlQuery } from "querystring";
 import { RegionJSON, Region } from "@actnowcoalition/regions";
 import { getRegionFromSlugStrict } from "src/utils/routing";
+import { MetricValue } from "@actnowcoalition/ui-components";
+import { MetricId } from "src/utils/metrics";
+import { Box } from "@mui/material";
 
 const ShareImagePage: NextPage<{ regionJSON: RegionJSON }> = ({
   regionJSON,
 }) => {
   const region = Region.fromJSON(regionJSON);
-  return <div>{region.regionId}</div>;
+  return (
+    <Box className="screenshot">
+      <div>{region.fullName}</div>
+      <MetricValue
+        className="screenshot-ready"
+        metric={MetricId.METRIC1}
+        region={region}
+      />
+    </Box>
+  );
 };
 
 interface RegionPageSlug extends ParsedUrlQuery {
   regionSlug: string;
 }
 
+// Use get static props so these are generated at build time s.t we dont use SSR
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   const { regionSlug } = params as RegionPageSlug;
   const region = getRegionFromSlugStrict(regionSlug);
   return { props: { regionJSON: region.toJSON() } };
 };
-
-// export const getStaticPaths: GetStaticPaths = async () => {
-//   const paths = regions.all.map((region) => ({
-//     params: { regionSlug: getRegionSlug(region) },
-//   }));
-//   return { paths, fallback: false };
-// };
 
 export default ShareImagePage;
