5835e8b650e6ac560d7424c3853c9922a03b9039
Pablo Navarro <pnavarrc@gmail.com>
2022-09-28 09:56:30 -0700

    Add starting content for the homepage

--- src/screens/Homepage/Homepage.tsx
+++ src/screens/Homepage/Homepage.tsx
@@ -1,10 +1,16 @@
 import { Container } from "@mui/material";
 import Typography from "@mui/material/Typography";
+import { Page } from "src/cms";
 
-const Homepage: React.FC = () => {
+const Homepage: React.FC<{ page: Page }> = ({ page }) => {
+  const { microcopy } = page;
   return (
     <Container>
-      <Typography variant="h1">Homepage</Typography>
+      <Typography variant="h1">{microcopy.get("title")}</Typography>
+      <Typography variant="h2">{microcopy.get("heading.title")}</Typography>
+      <Typography variant="paragraphLarge">
+        {microcopy.get("heading.intro")}
+      </Typography>
     </Container>
   );
 };
