2e0ee0e8d7dd327837897fa569ac45fc13ab3807
Sean McClure <sean.mcclure17@gmail.com>
2022-09-28 14:50:40 -0400

    Wire up screenshot url with hard-coded host

--- src/screens/Location/Location.tsx
+++ src/screens/Location/Location.tsx
@@ -3,6 +3,9 @@ import { Region } from "@actnowcoalition/regions";
 import { PageMetaTags } from "components/SocialMetaTags";
 
 export const Location: React.FC<{ region: Region }> = ({ region }) => {
+  const screenshotApiUrl =
+    "https://us-central1-test-url-api.cloudfunctions.net/api/dynamic-image";
+  const hostName = "act-now-template-95ktri6vr-covidactnow.vercel.app"; // FIX hardcoded url
   return (
     <>
       <PageMetaTags
@@ -10,7 +13,7 @@ export const Location: React.FC<{ region: Region }> = ({ region }) => {
         url={`/us/${region.shortName}`}
         title={`${region.shortName} Page`}
         description={`${region.shortName} Description`}
-        img={`/internal/share-image/${region.slug}/index.png`}
+        img={`${screenshotApiUrl}/https://${hostName}/internal/share-image/${region.slug}`}
       />
       <Container>
         <Typography variant="h1">{region.shortName}</Typography>
