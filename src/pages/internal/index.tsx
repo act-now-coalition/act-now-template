f20edafe2085dcddcf3a8717d050a068062663c1
Pablo Navarro <pnavarrc@gmail.com>
2022-10-13 16:55:23 -0700

    Minor updates to internal pages

---
+++ src/pages/internal/index.tsx
@@ -0,0 +1,41 @@
+import { NextPage } from "next";
+import {
+  Breadcrumbs,
+  Container,
+  Link,
+  List,
+  ListItem,
+  Typography,
+} from "@mui/material";
+import { PageMetaTags } from "components/SocialMetaTags";
+
+const InternalHome: NextPage = () => {
+  return (
+    <>
+      <PageMetaTags
+        siteName="Act Now Template"
+        title="Internal Home"
+        description="Directory of internal pages"
+        url="/internal"
+      />
+      <Container maxWidth="md" sx={{ py: 5 }}>
+        <Breadcrumbs aria-label="breadcrumb" sx={{ my: 2 }}>
+          <Link color="inherit" underline="hover" href="/">
+            Home
+          </Link>
+          <Link color="inherit" underline="hover" href="/internal">
+            Internal
+          </Link>
+        </Breadcrumbs>
+        <Typography variant="h1">Internal</Typography>
+        <List>
+          <ListItem>
+            <Link href="/internal/metrics">Metrics</Link>
+          </ListItem>
+        </List>
+      </Container>
+    </>
+  );
+};
+
+export default InternalHome;
