11fa1b450dd7c9e1ba90a3c31194cfe64648c835
Pablo Navarro <pnavarrc@gmail.com>
2022-10-17 14:33:39 -0700

    Implement /internal and minor updates to /internal/metrics (#42)

---
+++ src/pages/internal/index.tsx
@@ -0,0 +1,42 @@
+import { NextPage } from "next";
+import {
+  Breadcrumbs,
+  Container,
+  Link,
+  List,
+  ListItem,
+  Typography,
+} from "@mui/material";
+import { PageMetaTags } from "../../components/SocialMetaTags";
+import { cms } from "../../cms";
+
+const InternalHome: NextPage = () => {
+  return (
+    <>
+      <PageMetaTags
+        siteName={cms.settings.siteName}
+        title="Internal Home"
+        description="Directory of internal pages"
+        url="/internal"
+      />
+      <Container maxWidth="md" sx={{ py: 5 }}>
+        <Breadcrumbs aria-label="breadcrumb" sx={{ my: 2 }}>
+          <Link color="inherit" underline="hover" href="/">
+            Home
+          </Link>
+          <Link color="inherit" underline="hover" href="/internal">
+            Internal
+          </Link>
+        </Breadcrumbs>
+        <Typography variant="h1">Internal</Typography>
+        <List>
+          <ListItem>
+            <Link href="/internal/metrics">Metrics</Link>
+          </ListItem>
+        </List>
+      </Container>
+    </>
+  );
+};
+
+export default InternalHome;
--- src/pages/internal/metrics.tsx
+++ src/pages/internal/metrics.tsx
@@ -1,60 +1,99 @@
+import { useState } from "react";
 import type { NextPage } from "next";
+import {
+  Breadcrumbs,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Container,
+  Link,
+  Modal,
+  Paper,
+  Stack,
+  Typography,
+} from "@mui/material";
 import { useMetricCatalog } from "@actnowcoalition/ui-components";
 import { Metric } from "@actnowcoalition/metrics";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import {
-  Container,
-  List,
-  ListItem,
-  Typography,
-  Stack,
-  Accordion,
-  AccordionSummary,
-  AccordionDetails,
-} from "@mui/material";
+import { PageMetaTags } from "../../components/SocialMetaTags";
 import { MetricId } from "src/utils/metrics";
+import { cms } from "../../cms";
 
 const MetricsDirectory: NextPage = () => {
   const metricCatalog = useMetricCatalog();
 
   return (
-    <Container maxWidth="md" sx={{ py: 5 }}>
-      <Typography variant="h1">Metrics</Typography>
-      <List>
-        {metricCatalog.metrics.map(
-          (metricItem: Metric | MetricId, i: number) => {
-            const metric = metricCatalog.getMetric(metricItem);
-            return (
-              <ListItem divider={true} key={i}>
-                <Stack>
-                  <Typography variant="h3" component="h2" my={1.25}>
-                    {metric.name}
-                  </Typography>
-                  <Typography>ID: {metric.id}</Typography>
-                  <Typography>Name: {metric.name}</Typography>
-                  <Typography>Extended name: {metric.extendedName}</Typography>
-                  <Typography>
-                    Thresholds:{" "}
-                    {!metric.thresholds ? "N/A" : metric.thresholds.join(", ")}
-                  </Typography>
-                  <Accordion>
-                    <AccordionSummary
-                      expandIcon={<ExpandMoreIcon />}
-                      sx={{ backgroundColor: "#eee" }}
-                    >
-                      JSON
-                    </AccordionSummary>
-                    <AccordionDetails>
-                      <pre>{JSON.stringify(metric, null, 2)}</pre>
-                    </AccordionDetails>
-                  </Accordion>
-                </Stack>
-              </ListItem>
-            );
-          }
-        )}
-      </List>
-    </Container>
+    <>
+      <PageMetaTags
+        siteName={cms.settings.siteName}
+        title="Metrics"
+        description="Directory of available metrics"
+        url="/internal/metrics"
+      />
+      <Container maxWidth="md" sx={{ py: 5 }}>
+        {/* TODO: Implement a basic version in the packages repo */}
+        <Breadcrumbs aria-label="breadcrumb" sx={{ my: 2 }}>
+          <Link color="inherit" underline="hover" href="/">
+            Home
+          </Link>
+          <Link color="inherit" underline="hover" href="/internal">
+            Internal
+          </Link>
+          <Link color="text.primary" underline="hover" href="/internal/metrics">
+            Metrics
+          </Link>
+        </Breadcrumbs>
+
+        <Typography variant="h1">Metrics</Typography>
+        {metricCatalog.metrics.map((metricItem: Metric | MetricId) => {
+          const metric = metricCatalog.getMetric(metricItem);
+          return (
+            <MetricCard key={`metric-card-${metric.id}`} metric={metric} />
+          );
+        })}
+      </Container>
+    </>
+  );
+};
+
+const MetricCard = ({ metric }: { metric: Metric }) => {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  return (
+    <Card sx={{ my: 4 }}>
+      <CardContent>
+        <Stack>
+          <Typography variant="h3" component="h2" my={1.25}>
+            {metric.name}
+          </Typography>
+          <Typography>ID: {metric.id}</Typography>
+          <Typography>Name: {metric.name}</Typography>
+          <Typography>Extended name: {metric.extendedName}</Typography>
+          <Typography>
+            Thresholds:{" "}
+            {!metric.thresholds ? "N/A" : metric.thresholds.join(", ")}
+          </Typography>
+        </Stack>
+      </CardContent>
+      <CardActions>
+        <Button onClick={() => setModalOpen(true)}>See Definition</Button>
+        <Modal open={modalOpen} onClose={() => setModalOpen(false)}>
+          <Paper
+            sx={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              width: { xs: "100%", sm: 600 },
+              p: 4,
+            }}
+          >
+            <Typography variant="h2">{metric.name}</Typography>
+            <pre>{JSON.stringify(metric, null, 2)}</pre>
+          </Paper>
+        </Modal>
+      </CardActions>
+    </Card>
   );
 };
 
