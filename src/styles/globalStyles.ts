7d2b334430bdb58d6ef18cb01ab310899e4a3e57
Michael Lehenbauer <mikelehen@gmail.com>
2022-12-06 14:33:16 -0800

    Fix #174: Fix anchor link scrolling. (#63)
    * Fix #174: Fix anchor link scrolling.
    
    * CR Feedback: Use Global to inject global styles. Put scroll-padding-offset in AppBar and the rest in globalStyles.ts which is now imported into _app.tsx.
    
    * CR Feedback: Unify global styles in globalStyles.ts per Chelsi's feedback.

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -4,8 +4,12 @@ const globalStyles = css`
   html {
     scroll-behavior: smooth;
 
-    // AppBar height + fuzz
-    scroll-padding-top: 100px;
+    // The AppBar component (src/components/AppBar/AppBar.tsx) uses sticky
+    // positioning to always be at the top of the screen. To make sure that anchor
+    // links scroll into view appropriately we need to add extra scroll padding to
+    // compensate for the AppBar height.  You may need to adjust this padding if
+    // you make changes to the AppBar.
+    scroll-padding-top: 64px;
   }
 
   body {
