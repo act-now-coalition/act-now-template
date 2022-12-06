2c8b352cb976afa74a0122b8eeee15b6bd3033f2
Michael Lehenbauer <mikelehen@gmail.com>
2022-12-06 14:22:56 -0800

    CR Feedback: Unify global styles in globalStyles.ts per Chelsi's feedback.

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -3,6 +3,13 @@ import { css } from "@emotion/react";
 const globalStyles = css`
   html {
     scroll-behavior: smooth;
+
+    // The AppBar component (src/components/AppBar/AppBar.tsx) uses sticky
+    // positioning to always be at the top of the screen. To make sure that anchor
+    // links scroll into view appropriately we need to add extra scroll padding to
+    // compensate for the AppBar height.  You may need to adjust this padding if
+    // you make changes to the AppBar.
+    scroll-padding-top: 64px;
   }
 
   body {
