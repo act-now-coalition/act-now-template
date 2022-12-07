201093274075dbe57194e3718d5612f2847f4f0d
Fai <fai@covidactnow.org>
2022-12-07 14:22:51 -0800

    Move footer to homepage

--- src/components/Footer/Footer.style.tsx
+++ src/components/Footer/Footer.style.tsx
@@ -3,8 +3,11 @@ import styled from "@emotion/styled";
 import { theme } from "../../styles";
 
 export const StyledFooter = styled("footer")`
-  padding: ${theme.spacing(8, 3)};
   background-color: ${theme.palette.common.black};
+  width: 100%;
+  padding: ${theme.spacing(8, 3)};
+  position: fixed;
+  bottom: 0px;
 `;
 
 export const StyledLink = styled(Link)`
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,16 +1,55 @@
 import React from "react";
 import { Stack } from "@mui/material";
 import { StyledFooter } from "./Footer.style";
+import {
+  Box,
+  Typography,
+  IconButton,
+  useTheme,
+  ButtonGroup,
+  Link,
+} from "@mui/material";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import { StyledLink } from "./Footer.style";
 
-export interface FooterProps {
-  children: React.ReactNode;
-}
-
-const Footer: React.FC<FooterProps> = ({ children }) => {
+const Footer: React.FC = () => {
+  const theme = useTheme();
   return (
     <StyledFooter>
       <Stack spacing={3} maxWidth="960px" margin="auto">
-        {children}
+        <Box maxWidth={100}>
+          <Link href="/">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Bananas.svg/2560px-Bananas.svg.png"
+              width="100%"
+              alt="placeholder image"
+            />
+          </Link>
+        </Box>
+        <Typography variant="paragraphLarge" color={theme.palette.common.white}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sit
+          amet imperdiet lectus.
+        </Typography>
+        <Box sx={{ color: theme.palette.common.white }}>
+          <StyledLink href="/">About</StyledLink>
+          &nbsp;·&nbsp;
+          <StyledLink href="/">Contact</StyledLink>
+          &nbsp;·&nbsp;
+          <StyledLink href="/">Terms</StyledLink>
+        </Box>
+        <ButtonGroup disableRipple>
+          <IconButton edge="start" sx={{ color: theme.palette.common.white }}>
+            <FacebookIcon />
+          </IconButton>
+          <IconButton sx={{ color: theme.palette.common.white }}>
+            <TwitterIcon />
+          </IconButton>
+          <IconButton sx={{ color: theme.palette.common.white }}>
+            <InstagramIcon />
+          </IconButton>
+        </ButtonGroup>
       </Stack>
     </StyledFooter>
   );
