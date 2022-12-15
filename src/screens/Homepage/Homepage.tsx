975c4a741b804a2c389596dda6daf4454bc18174
Pablo Navarro <pnavarrc@gmail.com>
2022-12-15 10:37:39 -0800

    Improve the homepage template layout

--- src/screens/Homepage/Homepage.tsx
+++ src/screens/Homepage/Homepage.tsx
@@ -1,8 +1,11 @@
-import { Container } from "@mui/material";
-import Typography from "@mui/material/Typography";
+import { Typography } from "@mui/material";
 
+import { RegionSearch } from "@actnowcoalition/ui-components";
+
+import { PageContainer, PageSection, Placeholder } from "components/Containers";
 import { PageMetaTags } from "components/SocialMetaTags";
 import { Page, cms } from "src/cms";
+import { regions } from "src/utils/regions";
 
 const Homepage: React.FC<{ page: Page }> = ({ page }) => {
   const { microcopy, metaTags } = page;
@@ -17,13 +20,35 @@ const Homepage: React.FC<{ page: Page }> = ({ page }) => {
         socialImgWidth={metaTags.socialImgWidth}
         socialImgHeight={metaTags.socialImgHeight}
       />
-      <Container>
-        <Typography variant="h1">{microcopy.get("title")}</Typography>
-        <Typography variant="h2">{microcopy.get("heading.title")}</Typography>
-        <Typography variant="paragraphLarge">
-          {microcopy.get("heading.intro")}
-        </Typography>
-      </Container>
+      <PageContainer maxWidth="md">
+        <PageSection>
+          <Typography variant="h1" align="center">
+            {microcopy.get("title")}
+          </Typography>
+          <Typography variant="h2" align="center">
+            {microcopy.get("heading.title")}
+          </Typography>
+          <Typography
+            variant="paragraphLarge"
+            align="center"
+            sx={{ display: "block", my: 2 }}
+          >
+            {microcopy.get("heading.intro")}
+          </Typography>
+        </PageSection>
+        <PageSection>
+          <RegionSearch options={regions.all} regionDB={regions} />
+        </PageSection>
+        <PageSection>
+          <Placeholder />
+        </PageSection>
+        <PageSection>
+          <Placeholder />
+        </PageSection>
+        <PageSection>
+          <Placeholder />
+        </PageSection>
+      </PageContainer>
     </>
   );
 };
