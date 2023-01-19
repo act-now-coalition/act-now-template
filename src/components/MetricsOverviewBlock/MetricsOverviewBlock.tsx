8eaa890e865d6f21fa5b0bd6aef26f14e259d550
Pablo Navarro <pnavarrc@gmail.com>
2023-01-19 13:43:12 -0800

    Rename component + minor fixes

--- src/components/MetricsOverviewBlock/MetricsOverviewBlock.tsx
+++ src/components/MetricsOverviewBlock/MetricsOverviewBlock.tsx
@@ -1,4 +1,4 @@
-import { Grid, useMediaQuery, useTheme } from "@mui/material";
+import { Grid, Theme, useMediaQuery } from "@mui/material";
 
 import { Metric } from "@actnowcoalition/metrics";
 import { Region } from "@actnowcoalition/regions";
@@ -9,23 +9,25 @@ import {
   useMetricCatalog,
 } from "@actnowcoalition/ui-components";
 
-export interface MetricsOverviewProps {
+export interface MetricsOverviewBlockProps {
   /**
-   * Region to display the metrics for.
+   * Region represented by the overview block.
    */
   region: Region;
   /**
-   * List of metrics or metric IDs to show in the overview.
+   * Array of metrics represented by the overview block. The order in which
+   * the metrics are displayed will match the order of the array.
    */
   metrics: (string | Metric)[];
 }
 
-export const MetricsOverview = ({
+export const MetricsOverviewBlock = ({
   metrics: metricsOrIds,
   region,
-}: MetricsOverviewProps) => {
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+}: MetricsOverviewBlockProps) => {
+  const isMobile = useMediaQuery((theme: Theme) =>
+    theme.breakpoints.down("md")
+  );
 
   const metricCatalog = useMetricCatalog();
   const metrics = metricsOrIds.map((m) => metricCatalog.getMetric(m));
