5442dc149daf60b33a27cb2162890136921afd35
Fai Hegberg <fai.hegberg@gmail.com>
2023-01-25 13:52:50 -0800

    Address comments

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -2,7 +2,7 @@ import { Link } from "@mui/material";
 
 const Logo: React.FC = () => {
   return (
-    <Link href="/">
+    <Link href="/" display="flex">
       <img src="/placeholderlogo.png" width={70} height={56} alt="Site logo" />
     </Link>
   );
